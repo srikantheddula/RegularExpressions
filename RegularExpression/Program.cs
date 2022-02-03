0f96222e5d72aefdc37c4081de44f1488bf431ca
srikantheddula <you@example.com>
2022-02-03 22:32:30 +0530

    [srikanth]:adding welcome message regexp

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RegularExpression");
+        }
+    }
+}
